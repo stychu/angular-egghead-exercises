a0920d88685be5991be9fdcc8fd5e6c0023a0eee
stychu <tomaszstysial@o2.pl>
2015-08-31 15:06:59 +0200

    DONE

--- app/song.js
+++ app/song.js
@@ -2,17 +2,25 @@
 
 var app = angular.module('app', []);
 
-app.factory('SongText', function ()
-{
-    return {song: 'Soft Kitty, Warm Kitty, little ball of fur. Happy Kitty, Sleepy Kitty, purr, purr, purr...', newWord: 'purr'};
+app.factory('SongText', function () {
+    return {
+        song: 'Soft Kitty, Warm Kitty, little ball of fur. Happy Kitty, Sleepy Kitty, purr, purr, purr...',
+        newWord: 'purr'
+    };
 });
 
-app.controller('NewWordCtrl', function ($scope, SongText)
-{
+app.controller('NewWordCtrl', function ($scope, SongText) {
     $scope.data = SongText;
+
+    $scope.replaceWord = function (text, word, newWord) {
+        if (word != "") {
+            //var  re = RegExp(word, 'g');
+            return text.split(word).join(newWord);
+
+        } else return text;
+    };
 });
 
-app.controller('SongCtrl', function ($scope, SongText)
-{
+app.controller('SongCtrl', function ($scope, SongText) {
     $scope.data = SongText;
 });
