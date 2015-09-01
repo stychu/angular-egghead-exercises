664b7657a8f6d95804dba1e833e78f42c1697890
stychu <tomaszstysial@o2.pl>
2015-09-01 11:37:59 +0200

    DONE

--- app/mouse.js
+++ app/mouse.js
@@ -2,34 +2,34 @@
 
 var app = angular.module('app', []);
 
-app.directive('enterC', function () {
+app.directive('enter', function () {
     return function (scope, element, attrs) {
         element.bind('mouseenter', function () {
-            element.addClass(attrs.enterC);
+            element.addClass(attrs.enter);
         })
     }
 });
 
-app.directive('leaveC', function () {
+app.directive('leave', function () {
     return function (scope, element, attrs) {
         element.bind('mouseleave', function () {
-            element.removeClass(attrs.enterC);
+            element.removeClass(attrs.enter);
         })
     }
 });
 
-app.directive('enterS', function () {
+app.directive('clickOn', function () {
     return function (scope, element, attrs) {
         element.bind('click', function () {
-            element.addClass(attrs.enterS);
+            element.addClass(attrs.clickOn);
         })
     }
 });
 
-app.directive('leaveS', function () {
+app.directive('dbClickOn', function () {
     return function (scope, element, attrs) {
         element.bind('dblclick', function () {
-            element.removeClass(attrs.enterS);
+            element.removeClass(attrs.clickOn);
         })
     }
 });
