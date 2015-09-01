6fb334983261d6988d21ba21007679a2de1f2b92
stychu <tomaszstysial@o2.pl>
2015-09-01 11:08:31 +0200

    test errors

--- app/mouse.js
+++ app/mouse.js
@@ -1,3 +1,35 @@
 'use strict';
 
 var app = angular.module('app', []);
+
+app.directive('enterC', function () {
+    return function (scope, element, attrs) {
+        element.bind('mouseenter', function () {
+            element.addClass(attrs.enterC);
+        })
+    }
+});
+
+app.directive('leaveC', function () {
+    return function (scope, element, attrs) {
+        element.bind('mouseleave', function () {
+            element.removeClass(attrs.enterC);
+        })
+    }
+});
+
+app.directive('enterS', function () {
+    return function (scope, element, attrs) {
+        element.bind('click', function () {
+            element.addClass(attrs.enterS);
+        })
+    }
+});
+
+app.directive('leaveS', function () {
+    return function (scope, element, attrs) {
+        element.bind('dblclick', function () {
+            element.removeClass(attrs.enterS);
+        })
+    }
+});
