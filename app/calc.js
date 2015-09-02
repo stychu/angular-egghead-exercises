d77e8937fd000eaccf28850b1d1a50c211600e70
stychu <tomaszstysial@o2.pl>
2015-09-02 12:27:17 +0200

    DONE

--- app/calc.js
+++ app/calc.js
@@ -2,7 +2,12 @@
 
 var calcApp = angular.module('calcApp', []);
 
-calcApp.controller('SumCtrl', function ($scope)
+var calcAppElements = {};
+
+calcAppElements.controllers = {};
+calcAppElements.directives = {};
+
+calcAppElements.controllers.SumCtrl = function ($scope)
 {
     this.firstNumberSum = 5;
     this.secondNumberSum = 2;
@@ -14,9 +19,9 @@ calcApp.controller('SumCtrl', function ($scope)
 
     $scope.SumCtrl = this;
 
-});
+};
 
-calcApp.controller('SubCtrl', function ($scope)
+calcAppElements.controllers.SubCtrl = function ($scope)
 {
     this.firstNumberSub = 12;
     this.secondNumberSub = 1;
@@ -27,9 +32,9 @@ calcApp.controller('SubCtrl', function ($scope)
     };
     $scope.SubCtrl = this;
 
-});
+};
 
-calcApp.controller('MultiCtrl', function ($scope)
+calcAppElements.controllers.MultiCtrl = function ($scope)
 {
     $scope.firstNumberMult = 12;
     $scope.secondNumberMult = 2;
@@ -38,9 +43,9 @@ calcApp.controller('MultiCtrl', function ($scope)
     {
         $scope.multi = a * b;
     };
-});
+};
 
-calcApp.controller('DivideCtrl', function ($scope)
+calcAppElements.controllers.DivideCtrl = function ($scope)
 {
     $scope.firstNumberDiv = 15;
     $scope.secondNumberDiv = 5;
@@ -49,24 +54,27 @@ calcApp.controller('DivideCtrl', function ($scope)
     {
         $scope.divid = (a / b).toFixed(2);
     };
-});
+};
 
-calcApp.directive('multi', function ()
+calcAppElements.directives.multi = function ()
 {
     return {
         restrict: 'E', scope: {
             a: '@', b: '@', solve: '&'
-        }, template: ' <div id="multiButton" class="btn btn-info" ng-click="solve({a : a, b: b})">Multiple</div>'
+        }, template: ' <div class="btn btn-info" ng-click="solve({a : a, b: b})">Multiple</div>'
 
     };
-});
+};
 
-calcApp.directive('divide', function ()
+calcAppElements.directives.divide = function ()
 {
     return {
         restrict: 'A', scope: {
             a: '@', b: '@', solve: '&'
-        }, template: '<div id="divideButton" class="btn btn-default" ng-click="solve({a : a, b: b})">Divide</div>'
+        }, template: '<div class="btn btn-default" ng-click="solve({a : a, b: b})">Divide</div>'
     };
-});
+};
+
+calcApp.controller(calcAppElements.controllers);
+calcApp.directive(calcAppElements.directives);
 
