e307c9877d647dc591fdc5c79200a8c3b5ec9a1f
stychu <tomaszstysial@o2.pl>
2015-09-02 11:32:39 +0200

    DONE

--- app/calc.js
+++ app/calc.js
@@ -3,4 +3,22 @@
 var calcApp = angular.module('calcApp', []);
 
 //SumCtrl
+calcApp.controller('SumCtrl', function ($scope) {
+    this.firstNumber = 12;
+    this.secondNumber = 1;
+    this.addition = function (firstNumber, secondNumber) {
+        this.sum = firstNumber + secondNumber;
+        return this.sum;
+    };
+    return $scope.sumCtrl = this;
+});
 //SubCtrl
+calcApp.controller('SubCtrl', function ($scope) {
+    this.firstNumber = 12;
+    this.secondNumber = 1;
+    this.subtraction = function (firstNumber, secondNumber) {
+        this.sub = firstNumber - secondNumber;
+        return this.sub;
+    };
+    return $scope.subCtrl = this;
+});
