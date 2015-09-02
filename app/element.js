5c2005a7a502817ab9366e647b740ecee169b6da
stychu <tomaszstysial@o2.pl>
2015-09-02 13:03:39 +0200

    DONE

--- app/element.js
+++ app/element.js
@@ -4,21 +4,43 @@ var element = angular.module('elementApp', []);
 
 element.directive('findElement', function ()
 {
+    var msg = angular.element('<h4></h4>');
 
     //complete link function
     var link = function (scope)
     {
         //watch changes on input
 
+        scope.$watch('input', function (value,ele) {
+
+            if (value === "circle") {
+                msg.removeClass("text-danger");
+                msg.empty();
+                msg.addClass('circle');
+            }
+            else if (value === "square") {
+                msg.removeClass("text-danger");
+                msg.empty();
+                msg.addClass('square');
+            }
+            else {
+                msg.removeClass("square");
+                msg.removeClass("circle");
+                msg.text('You did not enter *circle* or *square*').addClass("text-danger");
+            }
+        })
     };
 
     return {
+        restrict: 'E',
         replace: true,
         template: '<div> <input class="form-control" type="text" ng-model="input" placeholder="Write circle or square"></div>',
         compile: function (templateElement)
         {
-
             //return link function
+            templateElement.append(msg);
+
+            return link;
         }
     };
 });
