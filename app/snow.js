ec29f356d05c7991c3dbabc8b987c895840e8220
stychu <tomaszstysial@o2.pl>
2015-09-02 10:46:49 +0200

    DONE

--- app/snow.js
+++ app/snow.js
@@ -7,7 +7,23 @@ app.controller('SnowCtrl', function ($scope)
     $scope.snow = [];
 
 //    addPetal function
+    $scope.addPetal = function () {
+        var petal = {petal: '*'};
+        $scope.snow.push(petal.petal);
+        return $scope.snow;
+    };
 //    removePetal function
+    $scope.removePetal = function () {
+        $scope.snow.pop();
+        return $scope.snow;
+    };
 });
 
 //    add enter directive
+app.directive('enter', function () {
+    return function (scope, element, attrs) {
+        element.bind("mouseenter", function () {
+            scope.$apply(attrs.enter);
+        })
+    }
+});
