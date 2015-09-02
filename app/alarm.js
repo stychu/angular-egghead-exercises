d5a3b74e0597f7824957247676482b47dc1679b7
stychu <tomaszstysial@o2.pl>
2015-09-02 10:49:14 +0200

    Not Done

--- app/alarm.js
+++ app/alarm.js
@@ -1,6 +1,56 @@
 var app = angular.module('app', []);
 
 //<!--add alarm directive-->
+app.directive('alarm', function () {
+    return {
+        restrict: 'A',
+        scope: {},
+        controller: function ($scope) {
+            this.addRed = function () {
+                alert('Red alarm!');
+            };
+            this.addGreen = function () {
+                alert('Green alarm!');
+            };
+            this.addYellow = function () {
+                alert('Yellow alarm!');
+            };
+        }
+
+    }
+});
 //<!--add red directive-->
+app.directive('red', function () {
+    return {
+        require: 'alarm',
+        link: function (scope, element, attrs, alarmCtrl) {
+            element.bind('click', function () {
+                alarmCtrl.addRed();
+            });
+
+        }
+    }
+});
 //<!--add yellow directive-->
+app.directive('yellow', function () {
+    return {
+        require: 'alarm',
+        link: function (scope, element, attrs, alarmCtrl) {
+            element.bind('dblclick', function () {
+                alarmCtrl.addYellow();
+            })
+        }
+    }
+});
 //<!--add green directive-->
+app.directive('green', function () {
+    return {
+        require: 'alarm',
+        link: function (scope, element, attrs, alarmCtrl) {
+            element.bind('mouseenter', function () {
+                alarmCtrl.addGreen();
+            });
+
+        }
+    }
+});
