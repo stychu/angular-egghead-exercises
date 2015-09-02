1be37c4969ed4f2df37945125dab974888cc634b
stychu <tomaszstysial@o2.pl>
2015-09-02 13:44:31 +0200

    DONE

--- app/app.js
+++ app/app.js
@@ -5,6 +5,45 @@
     var app = angular.module('app', ['ngRoute']);
 
     // add routing
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'homeCtrl'
+            })
+            .when('/details/:brand/:capacity', {
+                templateUrl: 'views/details.html',
+                controller: 'routeCtrl'
+            })
+            .when('/secret', {
+                templateUrl: 'views/secret.html'
+            })
+            .when('/inputs/:brand/:capacity', {
+                redirectTo: function ($routeParams) {
+                    if ($routeParams.brand === 'secret' || $routeParams.capacity === 'secret') {
+                        return "/secret"
+                    }
+                    return "/details/" + $routeParams.brand + '/' + $routeParams.capacity
+                }
+            })
+            .when('/inputs/:brand', {
+                redirectTo: function ($routeParams) {
+                    if ($routeParams.brand === 'secret') {
+                        return "/secret"
+                    }
+                    return '/'
+                }
+            })
+            .when('/inputs//:capacity', {
+                redirectTo: function ($routeParams) {
+                    if ($routeParams.capacity === 'secret') {
+                        return "/secret"
+                    }
+                    return '/'
+                }
+            })
+
+    });
 
     app.controller('homeCtrl', function ($scope)
     {
