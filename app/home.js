2f12844317bcd35c3fb56474844f6edcdb34dbf2
stychu <tomaszstysial@o2.pl>
2015-09-03 12:18:58 +0200

    DONE

--- app/home.js
+++ app/home.js
@@ -1,17 +1,37 @@
 'use strict';
 var homeApp = angular.module('homeApp', []);
 
-// add homeApp.provider 
-
-// add homeApp.config 
-
-// remove factory 
-homeApp.factory('Home', function() {
-    var exampleNum = 67;
+// add homeApp.provider
+homeApp.provider('Home', function () {
+    var type,type2;
     return {
-        color: 'red',
-        number: exampleNum % 2 ? 100 : 99
-    };
+        setColor: function (color) {
+            if (!color) {
+
+                return type = 'red';
+            }
+            type = color;
+        },
+        setNumber: function (number) {
+            type2 = number % 2 == 0 ? 99 : 100;
+        },
+        $get: function () {
+            return {
+                color: type,
+                number: type2
+            }
+        }
+    }
+});
+
+// add homeApp.config
+homeApp.config(function (HomeProvider) {
+    HomeProvider.setColor();
+    //HomeProvider.setColor('yellow');
+    //HomeProvider.setColor('green');
+    HomeProvider.setNumber(1);
+    //HomeProvider.setNumber(2);
+
 });
 
 homeApp.controller('HomeCtrl', function (Home)
