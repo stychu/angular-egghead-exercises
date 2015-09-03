2b78472fe8432c7a28ccb6b55ed09ae07d24bca7
stychu <tomaszstysial@o2.pl>
2015-09-03 11:30:25 +0200

    DONE

--- app/crossroads.js
+++ app/crossroads.js
@@ -8,6 +8,7 @@ app.config(function ($routeProvider)
         templateUrl: 'views/crossroads.html', controller: 'CrossroadsCtrl'
 
     }).when('/error', {
+        templateUrl: 'views/crossroads.html',
         resolve: {
             loadData: app.error
         }
@@ -15,6 +16,7 @@ app.config(function ($routeProvider)
         templateUrl: 'views/rightWay.html', controller: function ($scope)
         {
             // success message
+            $scope.alertMessage = 'Great job! You have reached your destination!';
 
         }, resolve: {
             loadData: app.success
@@ -22,12 +24,17 @@ app.config(function ($routeProvider)
     });
 });
 
-app.directive('error', function ()
+app.directive('error', function ($rootScope)
 {
     return {
         restrict: 'E', template: '<div class="text-center alert {{alertColor}}">{{alertMessage}}</div>', link: function (scope)
         {
             //complete error directive
+            $rootScope.$on('$routeChangeError', function () {
+                scope.alertMessage = 'Dead end! You went in the wrong direction!';
+                scope.alertColor = 'alert-danger';
+                scope.moved = true;
+            });
         }
     };
 });
@@ -48,12 +55,20 @@ var CrossroadsCtrl = app.controller('CrossroadsCtrl', function ($scope, $interva
 });
 
 
-CrossroadsCtrl.error = function ()
+CrossroadsCtrl.error = function ($rootScope, $q, $timeout)
 {
-
+    var defer = $q.defer();
+    $timeout(function () {
+        defer.reject();
+    }, 1000);
+    return defer.promise;
 };
 
-CrossroadsCtrl.success = function ()
+CrossroadsCtrl.success = function ($rootScope, $q, $timeout)
 {
-
+    var defer = $q.defer();
+    $timeout(function () {
+        defer.resolve();
+    }, 1000);
+    return defer.promise;
 };
